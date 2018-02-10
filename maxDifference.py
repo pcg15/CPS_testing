aeb81c1516daf8e8da23a6a6ac77c8ab9a25f81e
kjans123 <kj123@duke.edu>
2018-02-09 19:08:54 -0500

    assg4_findMaxDiff

---
+++ maxDifference.py
@@ -0,0 +1,10 @@
+def maxFindDiff(inputList):
+    diffList = []
+    for i in range(len(inputList)):
+        if i != (len(inputList)-1):
+
+            oneDiff = abs(inputList[i] - inputList[i+1])
+
+            diffList.append(oneDiff)
+    maxxVal = round(max(diffList),5)
+    return maxxVal
