ad0d557a837d758ec7089d20e88dcb2a6f1ed05c
kjans123 <kj123@duke.edu>
2018-02-09 20:00:07 -0500

    yml change: pytest to py.test. Added spaces in .py files after commas in lists

--- test_maxDiff.py
+++ test_maxDiff.py
@@ -1,11 +1,11 @@
 def test_MaxDiffr():
     from maxDifference import maxFindDiff
-    listV = [1,2,3,6,10]
+    listV = [1, 2, 3, 6, 10]
     maxxNum = maxFindDiff(listV)
     assert maxxNum == 4
     findPos = maxFindDiff(listV)
     assert findPos > 0
-    listV2 = [1.1,2.2,3.6,8.9]
+    listV2 = [1.1, 2.2, 3.6, 8.9]
     maxxNumFloat = maxFindDiff(listV2)
     assert maxxNumFloat == 5.3
 test_MaxDiffr()
