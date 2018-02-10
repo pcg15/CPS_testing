e946f715f1d2acfe6e60327f7c2218472ee4b2b4
Marianne Lee <ML273@DESKTOP-4RV2O0C.localdomain>
2018-02-09 20:25:06 -0500

    Tabs are not legal in pep8. :(

--- test_FindMinMax.py
+++ test_FindMinMax.py
@@ -8,23 +8,22 @@ def test_positive_numbers():
 
 
 def test_negative_numbers():
-	from FindMinMax import FindMinMax
-	list2 = list(range(-12, -1))
-	assert FindMinMax(list2) == (-12, -2)
+    from FindMinMax import FindMinMax
+    list2 = list(range(-12, -1))
+    assert FindMinMax(list2) == (-12, -2)
 
 
 def test_floats():
-	from FindMinMax import FindMinMax
-	list3 = np.arange(-1.0, 1.1, 0.1)
-	tup = FindMinMax(list3)
-	assert abs(tup[0] +1.0) < 0.00001
-	assert abs(tup[1] - 1.0) < 0.00001
+    from FindMinMax import FindMinMax
+    list3 = np.arange(-1.0, 1.1, 0.1)
+    tup = FindMinMax(list3)
+    assert abs(tup[0] +1.0) < 0.00001
+    assert abs(tup[1] - 1.0) < 0.00001
 
 
 def test_fractions():
-	from FindMinMax import FindMinMax
-	list4 = np.arange(1/3, 5/3, 1/3)
-	tup = FindMinMax(list4)
-	assert abs(tup[0]-1/3) < 0.00001
-	assert abs(tup[1]-5/3) < 0.00001
-
+    from FindMinMax import FindMinMax
+    list4 = np.arange(1/3, 5/3, 1/3)
+    tup = FindMinMax(list4)
+    assert abs(tup[0]-1/3) < 0.00001
+    assert abs(tup[1]-5/3) < 0.00001
