885b510e6af8d87419db26a0d4a33881164e4ef8
kjans123 <kj123@duke.edu>
2018-02-12 20:29:40 -0500

    changed maxDifference file pep8 modifications

--- maxDifference.py
+++ maxDifference.py
@@ -4,7 +4,8 @@ def maxFindDiff(inputList):
 
     :param inputList: List of input numbers. May be float or int
 
-    :returns: max difference between any two adjacent numbers from list (list[i]-list[i+1])
+    :returns: max difference between any two adjacent numbers from list
+    (list[i]-list[i+1])
     :raises ImportError: raises error if math function is not found
     :raises TypeError: raises error if any list element is a string
     :raises ValueError: raises error if input is numerical, but of wrong type
@@ -12,27 +13,30 @@ def maxFindDiff(inputList):
     import logging
 
 
-    logging.basicConfig(filename="maxDiffLogs.txt", format='%(levelname)s %(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
+    logging.basicConfig(filename="maxDiffLogs.txt", format='%(levelname)s \
+    %(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
     try:
         import math as mt
         logging.info('Succesfully imported math module')
     except ImportError:
         logging.warning('Unable to find math package')
-        print ("cant find package (most likely you have not activated your virtual env)")
+        print("cant find package (most likely you have not activated your virtual env)")
     diffList = []
     for i in range(len(inputList)):
         if i != (len(inputList)-1):
             try:
-                oneDiff = mt.copysign((inputList[i] - inputList[i+1]),1)
+                oneDiff = mt.copysign((inputList[i] - inputList[i+1]), 1)
                 logging.info('Found difference')
                 logging.debug('Difference =' + str(oneDiff))
             except TypeError:
-                logging.warning('Invalid input: data in list is not all numerical')
+                logging.warning('Invalid input: data in list \
+                is not all numerical')
                 print("attemping math operation on non-numerical input")
             except ValueError:
                 logging.warning('Invalid input: data in list is of wrong numerical type')
-                print("attempting to use an invalid numerical input (e.g., trying to find the square root of a negative number)")
+                print("attempting to use an invalid numerical input \
+                (e.g., trying to find the square root of a negative number)")
             diffList.append(oneDiff)
     maxxVal = round(max(diffList), 5)
     logging.info('Finished')
