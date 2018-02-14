b7bf7534fc752b56f0566ff7cd5f7aded3c2f785
Marianne Lee <ML273@DESKTOP-4RV2O0C.localdomain>
2018-02-14 12:47:30 -0500

    Added rudimentary functions for combined assignment04

---
+++ assignment04.py
@@ -0,0 +1,16 @@
+def main(list1, list2, list3)
+    print("Max Difference is: " + maxDifference(list1))
+    print("Summation Total is: " + summation(list2))
+    print("FindMinMax: " + FindMinMax(list3))
+
+
+def maxDifference(list1)
+    from maxDifference import maxDifference
+    
+    
+def summation(list2)
+    from summation import summation
+
+
+def FindMinMax(list3)
+    from FindMinMax import FindMinMax
---
+++ docs/modules.rst
@@ -0,0 +1,8 @@
+CPS_testing
+===========
+
+.. toctree::
+   :maxdepth: 4
+
+   maxDifference
+   test_maxDiff
