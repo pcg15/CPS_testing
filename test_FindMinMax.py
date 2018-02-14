ccce0b254997e57cb17dd3b5a2f431680a2c01b9
Marianne Lee <35433940+ML273@users.noreply.github.com>
2018-02-14 13:19:12 -0500

    Update tests to pull from the main function

--- test_FindMinMax.py
+++ test_FindMinMax.py
@@ -2,19 +2,19 @@ import numpy as np
 
 
 def test_positive_numbers():
-    from FindMinMax import FindMinMax
+    from assignment04 import FindMinMax
     list1 = list(range(1, 11))
     assert FindMinMax(list1) == (1, 10)
 
 
 def test_negative_numbers():
-    from FindMinMax import FindMinMax
+    from assignment04 import FindMinMax
     list2 = list(range(-12, -1))
     assert FindMinMax(list2) == (-12, -2)
 
 
 def test_floats():
-    from FindMinMax import FindMinMax
+    from assignment04 import FindMinMax
     list3 = np.arange(-1.0, 1.1, 0.1)
     tup = FindMinMax(list3)
     assert abs(tup[0] + 1.0) < 0.00001
@@ -22,7 +22,7 @@ def test_floats():
 
 
 def test_fractions():
-    from FindMinMax import FindMinMax
+    from assignment04 import FindMinMax
     list4 = np.arange(1/3, 5/3, 1/3)
     tup = FindMinMax(list4)
     assert abs(tup[0] - 1/3) < 0.00001
