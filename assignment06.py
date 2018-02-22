c7df0f4338720b98d94b612f69974af7729b09fc
pcg15 <pcg15@duke.edu>
2018-02-22 09:52:46 -0500

    remove maxDifference02.py and add assignment06.py

---
+++ assignment06.py
@@ -0,0 +1,18 @@
+class Assignment06:
+    def__init__(self, list_ = list_)
+        self.list_ = list_
+        self.summation = None
+        self.FindSummation()
+        self.MinMax = None
+        self.FindMinMax()
+        self.maxDiff = None
+        self.maxDifference()
+
+    def FindSummation(self):
+        """function to sum the components of a list
+
+        :returns: the sum of the complete list
+        :param: list components can be a float or integer
+        """
+        from summation import summation
+        self.summation = sum_list_
